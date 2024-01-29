ed7749d797fa959d53abcc31983ddc61b5f4b426
meheraz1100 <meheraz1100@gmail.com>
2024-01-29 08:23:28 +0600

    problem 01 solved

---
+++ orange-apple.js
@@ -0,0 +1,12 @@
+var myFullMoney = 1000;
+
+var oneKgApplePrice = 350;
+var oneKgOrangePrice = 350;
+
+var totalCost = oneKgApplePrice + oneKgOrangePrice;
+
+var shopkeeperWillReturnMe = myFullMoney - totalCost;
+
+console.log("Output is " + shopkeeperWillReturnMe);
+
+//Output is 300
