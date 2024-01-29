2f27aeb15f49683799f57f9fdb2b4b77cf9306b6
meheraz1100 <meheraz1100@gmail.com>
2024-01-29 08:42:23 +0600

    all problem solved

---
+++ getreminder.js
@@ -0,0 +1,8 @@
+var input = 119;
+
+var divider = 4;
+
+var reminder = 119 % 5;
+console.log("The reminder is :" + reminder);
+
+//The reminder is :4
---
+++ givetheaverage.js
@@ -0,0 +1,15 @@
+//Marks
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+
+var totalMark = math + biology + chemistry + physics + bangla;
+
+var averageMark = totalMark / 5;
+averageMark = averageMark.toFixed(2);
+console.log("Your Average Mark is : " + averageMark);
+
+//Your Average Mark is : 71.04
